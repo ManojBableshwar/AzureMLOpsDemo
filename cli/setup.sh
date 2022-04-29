11b13fa52f8c001e292d51712f67f7048d2f1bda
Manoj Bableshwar <mabables@microsoft.com>
2022-04-29 16:26:05 -0700

    start..

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,10 +1,4 @@
 az extension add -n ml -y
 
-GROUP=${{ github.event.inputs.name.GROUP }}
-
-LOCATION=${{ github.event.inputs.name.LOCATION }}
-
-WORKSPACE=${{ github.event.inputs.name.WORKSPACE }}
-
 az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
 
