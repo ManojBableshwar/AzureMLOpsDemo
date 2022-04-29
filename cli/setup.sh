ca80ab640aa057f441690f3fc58f59e2fcbc398e
Manoj Bableshwar <mabables@microsoft.com>
2022-04-29 16:58:42 -0700

    checkin...

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,4 +1,12 @@
 az extension add -n ml -y
 
-az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
+# Use defaults if not passed by workflow inputs
+
+GROUP1=${GROUP:-"rg-contoso-819prod"}
+
+LOCATION1=${LOCATION:-"eastus"}
+
+WORKSPACE1=${WORKSPACE:-"mlw-contoso-819prod"}
+
+az configure --defaults group=$GROUP1 workspace=$WORKSPACE1 location=$LOCATION1
 
