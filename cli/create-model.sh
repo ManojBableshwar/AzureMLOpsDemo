407e36140506a8ce7be28c52bb2b6728c90bbdcb
Manoj Bableshwar <mabables@microsoft.com>
2022-07-18 21:12:26 -0700

    Mon Jul 18 21:12:25 PDT 2022

--- cli/create-model.sh
+++ cli/create-model.sh
@@ -2,7 +2,7 @@ run_id=$1
 child_job_index=$2
 model_id_prefix=$3
 
-model_id="model_id_prefix-$run_id"
+model_id="$model_id_prefix-$run_id"
 model_version=1
 
 [[ -z "$run_id" ]] && { echo "run_id is empty" ; exit 1; }
