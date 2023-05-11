1604ce6d54de2d7e3eda2459cb4eef9a10252004
Manoj Bableshwar <mabables@microsoft.com>
2023-05-10 20:49:08 -0700

    hf model testing

--- cli/deploy-hf-model.sh
+++ cli/deploy-hf-model.sh
@@ -1,4 +1,4 @@
-
+set -x
 model_name=$1
 instance_type="Standard_DS3_v2"
 
@@ -18,16 +18,21 @@ az ml online-endpoint create --name $endpoint_name $ws_info|| {
 
 az ml online-deployment create --name $deployment_name --endpoint-name $endpoint_name \
 --set model=$model_info instance_type="$instance_type" --file src/online-endpoint/deploy.yml --all-traffic  || {
-    echo "deployment create failed..."; exit 1; 
+    echo "deployment create failed...";
+    echo "\n\naz ml online-deployment get-logs --name $deployment_name --endpoint-name $endpoint_name\n\n"
+    az ml online-deployment get-logs --name $deployment_name --endpoint-name $endpoint_name
+    az ml online-endpoint delete --name $endpoint_name
+    exit 1; 
 }
 
-print("\n\az ml online-endpoint show --name $endpoint_name\n\n")
+echo "\n\az ml online-endpoint show --name $endpoint_name\n\n"
 az ml online-endpoint show --name $endpoint_name
-print("\n\az ml online-deployment show --name $deployment_name --endpoint-name $endpoint_name\n\n")
+echo "\n\az ml online-deployment show --name $deployment_name --endpoint-name $endpoint_name\n\n"
 az ml online-deployment show --name $deployment_name --endpoint-name $endpoint_name
-print ("\n\naz ml online-deployment get-logs --name $deployment_name --endpoint-name $endpoint_name\n\n")
+echo "\n\naz ml online-deployment get-logs --name $deployment_name --endpoint-name $endpoint_name\n\n"
 az ml online-deployment get-logs --name $deployment_name --endpoint-name $endpoint_name
 
-#az ml online-endpoint invoke --name $endpoint_name --deployment-name $deployment_name --request-file src/online-endpoint/sample.json $ws_info
-az ml online-endpoint delete --name $endpoint_name
+# todo add sample inference
+# az ml online-endpoint invoke --name $endpoint_name --deployment-name $deployment_name --request-file ...
 
+az ml online-endpoint delete --name $endpoint_name
